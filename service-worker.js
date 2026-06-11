13da7bbd8ff1f228b72002db02a282b3f2f33b90
DGutberlet-Creator <m6jv4f545d@privaterelay.appleid.com>
2026-06-11 20:56:36 +0100

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,16 +1,41 @@
-const CACHE_NAME = 'panini-2026-tracker-v1';
-const FILES_TO_CACHE = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+// A very simple service worker for offline support.  It caches the core assets on
+// install and serves them from cache when offline.  Feel free to extend this file
+// to cache images or other resources as you update your album.
+
+const CACHE_NAME = 'panini-2026-cache-v1';
+const ASSETS = [
+  '.',
+  'index.html',
+  'script.js',
+  'data.js',
+  'manifest.json',
+  'icons/icon-192.png',
+  'icons/icon-512.png'
+];
 
 self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE)));
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
 self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.map(key => key !== CACHE_NAME ? caches.delete(key) : null))));
-  self.clients.claim();
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+      );
+    })
+  );
 });
 
 self.addEventListener('fetch', event => {
-  event.respondWith(caches.match(event.request).then(resp => resp || fetch(event.request)));
+  // Always try cache first, then network
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
 });
